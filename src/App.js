a3512267286add9f0344bd2a178c7b392c52f16c
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-10-12 23:42:36 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+
 
 const App = () => {
-    return(
-        <div>
-        Your travel companion
-        </div>
-    );
-}
+ 
+
+  return (
+    <>
+     
+    </>
+  );
+};
+
 export default App;
