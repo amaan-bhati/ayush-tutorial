35ad6a36fa336247530f600a261d69a4d9451f9d
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-10-12 23:43:28 +0530

    grid for the list

--- src/App.js
+++ src/App.js
@@ -8,7 +8,22 @@ const App = () => {
 
   return (
     <>
+    
+  
+    <Grid container spacing={3} style={{ width: '100%' }}>
+      <Grid item xs={12} md={4}>
+        <List
+          isLoading={isLoading}
+          childClicked={childClicked}
+          places={filteredPlaces.length ? filteredPlaces : places}
+          type={type}
+          setType={setType}
+          rating={rating}
+          setRating={setRating}
+        />
+      </Grid>
      
+    </Grid>
     </>
   );
 };
