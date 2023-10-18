893f8daac335919d23ba61e877061e5ab0dde24f
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-10-18 23:04:36 +0530

    update map

--- src/components/Header/Map/Map.js
+++ src/components/Header/Map/Map.js
@@ -4,7 +4,12 @@ import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab/Rating';
 
+import mapStyles from '../../mapStyles';
+import useStyles from './styles.js';
 
+const Map = ({ coords, places, setCoords, setBounds, setChildClicked, weatherData }) => {
+  const matches = useMediaQuery('(min-width:600px)');
+  const classes = useStyles();
 
   return (
     <div className={classes.mapContainer}>
@@ -50,6 +55,6 @@ import Rating from '@material-ui/lab/Rating';
       </GoogleMapReact>
     </div>
   );
-;
+};
 
 export default Map;
